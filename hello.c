9978297e2dc224156b8211e6f0a8e67e2524e059
devp2 <devp2@mcmaster.ca>
2023-06-28 14:45:54 -0400

    changes

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+	printf("hello class");
+	return 0;
+}
---
+++ limit.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <limits.h>
+int main () {
+int value = INT_MAX + 1;
+printf (" Value : %d\n", value ) ;
+return 0;
+}
